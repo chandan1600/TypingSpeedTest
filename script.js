b92c6a86872902f8c0a07080b4afc5e7495d813d
Chandan Aulakh <49302381+chandan1600@users.noreply.github.com>
2020-08-13 18:40:30 +1200

    added timer reset

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
 timer = [0,0,0,0];
+var interval;
+var timerRunning = false;
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 function zero(time){
@@ -28,31 +30,40 @@ function beginTimer(){
 // Match the text entered with the provided text on the page:
 function spellChecker(){
     let enteredText = testArea.value;
-    let matchOriginText = originText.substring(0, enteredText);
+    let matchOriginText = originText.substring(0, enteredText.length);
 
-    if(enteredText==matchOriginText){
-        testWrapper.style.borderColor = "#1E90FF"
-    }
-    else if(enteredText==originText){
-        testWrapper.style.borderColor = "#32CD32"
+    if(enteredText == originText){
+        testWrapper.style.borderColor = "#32CD32";
+        clearInterval(interval);
     }
     else{
-        testWrapper.style.borderColor = "#FFA500"
+        if(enteredText == matchOriginText){
+            testWrapper.style.borderColor = "#1E90FF";
+        } else{
+        testWrapper.style.borderColor = "#FFA500";
+        }
     }
-
 }
 
 // Start the timer:
 function startTimer(){
     let textTypedLength = testArea.value.length;
-    if(textTypedLength === 0){
-        setInterval(beginTimer,10)
+    if(textTypedLength === 0 && !timerRunning){
+        timerRunning = true;
+        interval = setInterval(beginTimer,10);
     }
+    
 }
 
 // Reset everything:
 function reset(){
-    console.log("reset button pressed")
+    clearInterval(interval);
+    interval = null;
+    timer = [0,0,0,0];
+    timerRunning = false;
+    testArea.value = "";
+    theTimer.innerHTML = "00:00:00";
+    testWrapper.style.borderColor = "grey";
 }
 
 // Event listeners for keyboard input and the reset button:
